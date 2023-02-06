08a681aba5ebf7531320043185c2c3266b0d0ff4
Igor Stepanov <stepanovigor2110@gmail.com>
2023-02-06 20:07:02 +0200

    3th commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Header from "./components/Header";
 import MainTemp from "./components/MainTemp";
 import WeatherOnWeek from "./components/WeatherOnWeek";
+import axios from "axios";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({})
+
+  useEffect(() => {
+    const getWeatherData = async() => {
+      const { data } = await axios.get('http://api.weatherapi.com/v1/current.json?key=0620d2230b2c45d2a71193236222805&q=London&aqi=no')
+      console.log(data)
+    }
+    getWeatherData()
+
+  })
   return (
     <div className="App">
       <div className="container m-auto flex flex-col max-w-[420px] h-[100vh] p-6">
